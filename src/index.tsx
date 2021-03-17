98a5c4262338c32a12d3142b7a64f8cc4c209cd1
masuda <masuda.csp@gmail.com>
2021-03-17 13:17:03 +0900

    eslintとprettierを導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <div>Hello Preact</div>,
-    document.getElementById("app")!
-)
+  <div>Hello Preact</div>,
+  document.getElementById('app') as HTMLElement
+);
